01bb192d83aae632001bef15395471cb5f375fdb
chenxin <lightnoway@qq.com>
2020-10-16 18:06:20 +0800

    after docker-compose up

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '768dc6356aacc362408121241525d2f9'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7cd4355f-2c5b-42a0-9a48-99f60096f3b6'
+};
